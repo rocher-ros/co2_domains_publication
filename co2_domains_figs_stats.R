80a1a38fd29bd416b266d847fc145f93ff706cb9
Gerard Rocher-Ros <g.rocher.ros@gmail.com>
2019-03-06 09:48:57 +0100

    Update co2_domains_figs_stats.R

--- co2_domains_figs_stats.R
+++ co2_domains_figs_stats.R
@@ -4,10 +4,10 @@
 #### AUTHORS:  Gerard Rocher-Ros, Ryan Sponseller, William Lidber2, Carl-Magnus Mörth, Reiner Giesler
 #### Author of the script and contact email: Gerard Rocher-Ros (g.rocher.ros@gmail.com)
 ####
-#### Last edit: 2019-03-04
-#### Before running the script, dowload the datasets:
+#### Last edit: 2019-03-06
+#### Before running the script, download the datasets, deposited in the Swedish National Data Service:
 #### Dataset Miellajokka can be found here: https://doi.org/10.5878/pxa2-vy55 
-#### Global dataset can be found here: 
+#### Global dataset can be found here: https://doi.org/10.5878/77ps-4f21
 ####
 #### The final publication figures and pannels were prepared using Adobe Illustrator
 ####################################################################################
